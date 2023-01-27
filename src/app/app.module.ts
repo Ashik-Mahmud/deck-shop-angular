807d3e4a2829e650effc0a4921d5737655c9a1f4
Äshik Mahmud <ashikmamud187@gmail.com>
2023-01-28 03:10:22 +0600

    done to design car page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RegisterComponent } from './auth/register/register.component';
 import { CartComponent } from './components/cart/cart.component';
 import { FormsModule } from '@angular/forms';
 import { AuthModule } from './auth/auth.module';
+import { CartItemComponent } from './components/cart/cart-item/cart-item.component';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { AuthModule } from './auth/auth.module';
     LoginComponent,
     RegisterComponent,
     CartComponent,
+    CartItemComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, AuthModule],
   providers: [],
