b42552ffd878f747c16c3d2e030e9c419f87b662
Äshik Mahmud <ashikmamud187@gmail.com>
2023-01-28 02:56:08 +0600

    done to working with register page: +

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  name: string = '';
+  email: string = '';
+  password: string = '';
+  constructor() {}
 
+  onSubmit() {}
 }
